6a47e0906cfc263ab8af5c1ee22c2542156c8e9a
quocAnhnguyen1 <Quoc.Nguyen@Student.htw-berlin.de>
2023-04-21 11:57:03 +0200

    init

---
+++ src/main/java/htwberlin/webtech/LernappApplication.java
@@ -0,0 +1,13 @@
+package htwberlin.webtech;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LernappApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LernappApplication.class, args);
+	}
+
+}
---
+++ src/test/java/htwberlin/webtech/LernappApplicationTests.java
@@ -0,0 +1,13 @@
+package htwberlin.webtech;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LernappApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
